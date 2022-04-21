a564f8a0944397e6e7a0e9a2903af61180272f80
Pablo Cavalcante <pablocavalcanteh@gmail.com>
2022-04-21 02:08:06 -0300

    Moment edition concluded.

--- src/app/components/pages/edit-moment/edit-moment.component.ts
+++ src/app/components/pages/edit-moment/edit-moment.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 
 import { IMoment } from 'src/app/IMoment';
 import { MomentService } from 'src/app/services/moment.service';
+import { MessagesService } from 'src/app/services/messages.service';
 
 @Component({
   selector: 'app-edit-moment',
@@ -15,13 +16,31 @@ export class EditMomentComponent implements OnInit {
   btnText: string = "Editar"
 
   constructor(private momentService: MomentService,
-              private route: ActivatedRoute
+              private route: ActivatedRoute,
+              private messagesService: MessagesService,
+              private router: Router
               ) { }
 
   ngOnInit(): void {
 
     const id = Number(this.route.snapshot.paramMap.get("id"))
     this.momentService.getMomentById(id).subscribe(response => (this.moment = response.data))
+  }
+
+  async editHandler(moment: IMoment) {
+    const id = this.moment.id
+    const formData = new FormData()
+    formData.append('title', moment.title)
+    formData.append('description', moment.description)
+
+    if (moment.image) {
+      formData.append('image', moment.image)
+    }
+
+    await this.momentService.updateMoment(id!, formData).subscribe()
+
+    this.messagesService.add('Momento atualizado com sucesso!')
+    this.router.navigate(['/'])
   }
 
 }
